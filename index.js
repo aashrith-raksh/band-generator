5e5273b2153f122e7fee7a23094e1da384fbbc07
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-03-26 19:28:20 +0530

    implemented all routes

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
+app.use(express.static("public"))
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-
+  res.render("index.ejs")
 });
 
 app.post("/submit", (req, res) => {
-  
+  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+
+  res.render("index.ejs", {
+    adjective:randomAdj, 
+    noun: randomNoun
+  })
 });
 
 app.listen(port, () => {
